df2ce800b188722025440c098056d05f7b9df8ab
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2017-09-19 23:05:30 +0200

    Command Line Processing

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.CommandLineUtils;
 
 namespace Conway
 {
@@ -7,6 +9,27 @@ namespace Conway
         static void Main(string[] args)
         {
             Console.WriteLine("Conway - Game of Life");
+            var app = new CommandLineApplication();
+            app.Name = "Conway";
+            app.HelpOption("-?|-h|--help");
+            app.VersionOption("-v|--version","Conway 1.0.0");
+
+
+            var inputFile = app.Option("-i|--input <inputfile>","Input file",CommandOptionType.SingleValue);
+
+            app.OnExecute(()=>{
+                try{
+                    if(!inputFile.HasValue())
+                        throw new Exception("Please, specify an input file");
+                    if(!File.Exists(inputFile.Value()))
+                        throw new Exception("Please, check that the specified file exists");
+                }catch(Exception e){
+                    Console.WriteLine($"Error: {e.Message}");
+                }
+                return 0;
+            });
+
+            app.Execute(args);
         }
     }
 }
