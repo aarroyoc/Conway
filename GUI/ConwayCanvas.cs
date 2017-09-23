d31036157345d228b8db399ab87ba2ecec4261b9
Daniel <danielbazacovelasco@protonmail.com>
2017-09-23 11:53:23 -0400

    ConwayMatrix se imprime gráficamente y método de Iteración simple creado. Para hacer pruebas he puesto un Constructor temporal en el método render de ConwayCanvas

--- GUI/ConwayCanvas.cs
+++ GUI/ConwayCanvas.cs
@@ -2,13 +2,57 @@ using Avalonia;
 using Avalonia.Controls;
 using System;
 using Avalonia.Media;
-
+using Conway.Matrix; 
 namespace Conway.GUI{
     /*
     ConwayCanvas es la control que muestra la simulación en tiempo real
      */
     public class ConwayCanvas : Control{
-        public override void Render(DrawingContext context)
+
+        private  ConwayMatrix matrix=null;
+        public override void Render(DrawingContext context){
+
+            if (matrix==null){ //TEMPORAL
+                this.matrix= new ConwayMatrix();
+                this.matrix.SetSize(8,8);
+                this.matrix[1,0]=true;
+                this.matrix[1,1]=true;
+                this.matrix[1,2]=true;
+                this.Width=this.matrix.GetMatrix()[0].Count*10;
+                this.Height=this.matrix.GetMatrix().Count*10;
+
+
+
+                Console.WriteLine(this.matrix.ToString());
+
+
+                this.matrix.Iterate();
+                
+                Console.WriteLine(this.matrix.ToString());
+            }
+           
+
+              for (int y=0;y<this.matrix.GetMatrix().Count;y++){
+                ;
+                    for (int x=0;x<this.matrix.GetMatrix()[0].Count;x++){
+                        var rect=new Rect(x*10,y*10,10,10);
+                     
+                     
+                        if (this.matrix.GetMatrix()[y][x]==true){
+                                context.FillRectangle(Brushes.Black,rect);
+                            
+                        }
+                        else{
+
+                            context.FillRectangle(Brushes.Red,rect);
+                        }
+                       
+    
+            
+                 }
+          }
+        }
+        /* 
         {
             int x = 0;
             int y = 0;
@@ -22,6 +66,8 @@ namespace Conway.GUI{
                 }
             }
         }
+ */
+    
         public ISolidColorBrush RandomColor(){
             var r = new Random().Next(0,5);
             var color = new ISolidColorBrush []{Brushes.Red,Brushes.Blue,Brushes.Green,Brushes.Yellow,Brushes.Purple};
--- Matrix/ConwayMatrix.cs
+++ Matrix/ConwayMatrix.cs
@@ -9,6 +9,71 @@ namespace Conway.Matrix {
         {
             matrix = new List<List<bool>>();
         }
+        public List<List<bool>> GetMatrix(){
+            return this.matrix;
+
+        }
+
+        public void Iterate(){
+            //Para que las casillas no afecten al resto, dejo los cambios en un buffer.
+            //Problemas: Las casillas fuera del tablero no se verifican, es decir si  [0,0].[0,1][0,2] están activas, [1,-1] debería activarse.
+
+            List<List<int>> Buffer=new List<List<int>>(); //Temporal
+            for(int x=0;x<this.matrix.Count;x++){
+                for (int y=0;y<this.matrix.Count;y++){
+                    bool alive=this[x,y];
+
+                    int aliveNear=0;
+                for (int i=-1;i<2;i++){
+                    for (int z=-1;z<2;z++){
+                        if (!(z==0 & i==0)){
+                            aliveNear+=Convert.ToInt32(this[x+i,y+z]); //Posible optimización pequeña: En el momento en el que 
+                                                                            //Alivenear>3, salir del bucle
+                        if (x==0 & y==1){
+                            Console.WriteLine("0,1 PRUEBA "+i+" "+ z + "-->"+ Convert.ToInt32(this[x+i,y+z]));
+                        }
+                        }
+
+                    }
+                }    
+                if (x==0 & y==1){
+                    Console.WriteLine("0,1"+aliveNear+"¿Alive?"+alive);
+                }
+                if (alive==true){
+                    if (aliveNear!=3 & aliveNear!=2){
+                        List<int> temp=new List<int>{
+                            x,y,0 //(x,y, estado al que va a pasar)
+                        };
+                      Buffer.Add(temp);
+                    }
+                }
+                else{
+                      if (x==0 & y==1){
+                         Console.WriteLine("dasdsadasdfñlasfñljasdfñls"+aliveNear+"¿Alive?"+alive);
+                      }
+                      if (aliveNear==3){
+                        List<int> temp=new List<int>{
+                            x,y,1 //(x,y, estado al que va a pasar)
+                        };
+                        Buffer.Add(temp);
+                        Console.WriteLine("UNA CASILLA HA PASADO A LA VIDAAAAAAAAAA");
+               
+
+                }  
+
+                }
+
+            }
+        }
+
+        foreach(List<int> casilla in Buffer){
+            Console.WriteLine("CAMBIOOOOOOOO"+casilla[0]+" "+casilla[1] + "  ---> " + casilla[2]);
+            this.matrix[casilla[0]][casilla[1]]=Convert.ToBoolean(casilla[2]);
+        
+        }
+}
+
+
         public void SetSize(int x, int y)
         {
             for(int i=matrix.Count;i<x;i++){
@@ -23,7 +88,7 @@ namespace Conway.Matrix {
             var sb = string.Empty;
             foreach(var c in matrix){
                 foreach(var e in c){
-                    sb += e ? "X" : ".";
+                    sb += e ? "X" : "."; //¿?
                 }
                 sb += System.Environment.NewLine;
             }
@@ -51,13 +116,13 @@ namespace Conway.Matrix {
                     Console.WriteLine($"X: {x}\tCount: {matrix.Count}");
                     if(x<0){
                         // insert
-                        matrix.Insert(0,new List<bool>());
+                        matrix.Insert(0,new List<bool>()); 
                         // crear Y
-                        for(int j=0;j<matrix[1].Count;j++){
-                            matrix[0].Add(false);
+                        for(int j=0;j<matrix[1].Count;j++){ 
+                            matrix[0].Add(false);          
                         }
                         xplus = 1;
-                    }else if(x==matrix.Count){
+                    }else if(x==matrix.Count){ 
                         Console.WriteLine("Ampliando X");
                         // add
                         matrix.Add(new List<bool>());
